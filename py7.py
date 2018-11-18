37346f89fedb42660fd203b90d43a84550246e5e
likaon100 <luk.podlewski@gmail.com>
2018-11-18 22:42:30 +0100

    a

--- py7.py
+++ py7.py
@@ -7,19 +7,48 @@
 #     if litera == "e":
 #         print("to jest e!")
 
-#range genereuje liste na podstawie liczby podanej
+# range genereuje liste na podstawie liczby podanej
 
 # for i in range(0, 51,2):
 #     print(i)
 # ____________________________________________________________________9
-fruits = ['apple', 'orange', 'pear', 'banana', 'apple']
+# fruits = ['apple', 'orange', 'pear', 'banana', 'apple']
+#
+#
+# i = 0
+# print ("start pętli {} {}".format("yollo", "elo"))
+# for i, fruit in enumerate(fruits):
+#     if i==3:
+#         break
+#     print(i)
+#     print("{} jest git".format(fruit))
+# print("koniec")
 
+# ____________________________________________________________________10
 
-i = 0
-print ("start pętli {} {}".format("yollo", "elo"))
-for i, fruit in enumerate(fruits):
-    if i==3:
-        break
-    print(i)
-    print("{} jest git".format(fruit))
-print("koniec")
+# fruits = ['apple', 'orange', 'pear', 'banana', 'apple']
+# print("start")
+#
+# for fruit in fruits:
+#     print(fruit)
+#     if fruit == 'orange':
+#         continue
+#     if fruit == 'banana':
+#         break
+#
+# print("koniec")
+# ____________________________________________________________________11
+
+# fruits = ['apple', 'orange', 'pear', 'banana', 'apple', 'strawberry', 'apple', 'berry']
+#
+# if "appaa" in fruits:
+#     print ("jest japko")
+# elif 'orange' in fruits:
+#     print("uraaa")
+# #jeśli nie
+#     if 'orange' in fruits:
+#         print ('jes pomarancza')
+#     else:
+#         print("ni ma nic")
+
+# operatory logiczne___________________11
---
+++ pythonyoutue11.py
@@ -0,0 +1,13 @@
+# liczba = int(input())
+#
+# if liczba > 20 and liczba > 5:
+#     print('tak')
+# else:
+#     print('nie')
+
+#_______________ operator or - jeden z warunkow musi byc prwadziwy
+liczba = [1,2,3,4,5,6,7]
+if not 0 in liczba:
+    print('tak')
+else:
+    print('nie')
